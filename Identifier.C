1f9ff6ba482e12a7d77ccc2dcd782db460d3c64f
Siri2503 <113344836+Siri2503@users.noreply.github.com>
2023-04-24 13:53:20 +0530

    Create Identifier.C

---
+++ Identifier.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+int main()
+{
+    char c[10];
+
+    printf("Enter a string: ");
+    scanf("%c", &c);
+
+    if (isalpha(c[0]) == 0)
+         printf("%c is not an identifier.", c);
+    else
+         printf("%c is an identifier.", c);
+
+    return 0;
+}
